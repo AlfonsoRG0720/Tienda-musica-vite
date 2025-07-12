c0f59aac1d07d497e8fd3984814e9f3ddc05886b
AlfonsoRG0720 <alfonsor814@gmail.com>
2025-07-12 21:30:05 +0200

    Modificaciones para el testing de github workflow

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+// jest.config.ts
+import type { JestConfigWithTsJest } from 'ts-jest'
+
+const config: JestConfigWithTsJest = {
+  preset: 'ts-jest/presets/default-esm',
+  verbose: true,
+  testEnvironment: 'jsdom',
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { useESM: true }],
+  },
+  moduleNameMapper: {
+    // opcional: mapea cualquier CSS o asset si importas esos archivos
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  roots: ['<rootDir>/__test__'],
+}
+
+export default config
